d9c39c8c964aa665d51a1b4a83223680f3539295
KYEONGMIN PARK <mss4236@naver.com>
2021-01-30 23:07:23 +0900

    change ch6 folder

--- backend/routes/post.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res) => {});
-router.post("/images", (req, res) => {});
-
-module.exports = router;
--- front/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all, fork } from "redux-saga/effects";
-import user from "./user";
-import post from "./post";
-
-export default function* rootSaga() {
-  yield all([fork(user), fork(post)]);
-}
